0899752712ee4127fabd75f82164b7478e66032f
Seifeddine-Selmi <selmi.seifeddine19@gmail.com>
2017-03-17 16:39:39 -0400

    Creating Bootstrap Style Datepicker

--- backend/models/Companies.php
+++ backend/models/Companies.php
@@ -13,6 +13,7 @@ use Yii;
  * @property string $company_address
  * @property string $company_created_date
  * @property string $company_status
+ * @property string $company_start_date
  *
  * @property Branches[] $branches
  * @property Departments[] $departments
@@ -35,6 +36,7 @@ class Companies extends \yii\db\ActiveRecord
         return [
             [['company_name', 'company_email', 'company_address', 'company_status'], 'required'],
             [['company_created_date'], 'safe'],
+            [['company_start_date'], 'safe'],
             [['company_status'], 'string'],
             [['company_name', 'company_email'], 'string', 'max' => 100],
             [['company_address'], 'string', 'max' => 255],
@@ -53,6 +55,7 @@ class Companies extends \yii\db\ActiveRecord
             'company_address' => 'Company Address',
             'company_created_date' => 'Company Created Date',
             'company_status' => 'Company Status',
+            'company_start_date' => 'Company Start Date',
         ];
     }
 
--- backend/views/companies/_form.php
+++ backend/views/companies/_form.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use dosamigos\datepicker\DatePicker;
 
 /* @var $this yii\web\View */
 /* @var $model backend\models\Companies */
@@ -19,6 +20,19 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'company_address')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'company_status')->dropDownList([ 'active' => 'Active', 'inactive' => 'Inactive', ], ['prompt' => 'Status']) ?>
+
+
+    <?= $form->field($model, 'company_start_date')->widget(
+        DatePicker::className(), [
+        // inline too, not bad
+        'inline' => false,
+        // modify template for custom rendering
+        //'template' => '<div class="well well-sm" style="background-color: #fff; width:250px">{input}</div>',
+        'clientOptions' => [
+            'autoclose' => true,
+            'format' => 'yyyy-m-d'
+        ]
+    ]);?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
